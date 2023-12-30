5698e599500c9fea651e421a3b699ef10959a4f5
Hojeong016 <skflsehdn@naver.com>
2023-12-30 14:06:28 +0900

    1장

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,14 @@
 public class Calculator { // Calculator 클래스
 //'상태'
 //메소드
-//public static void main(String[] args) {
-//    int a = subtract(2,1);
-//    int b = multiply(2,1);
-//    int c = divide(30,5);
-//
-//    System.out.println(a+b+c);
-//
-//}
+public static void main(String[] args) {
+   int a = subtract(2,1);
+   int b = multiply(2,1);
+   int c = divide(30,5);
+
+    System.out.println(a+b+c);
+
+}
     public static int subtract(int num1, int num2){
        int sum;
        sum= num1 - num2;
